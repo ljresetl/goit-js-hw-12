832955b047a7b89a4830ab4ce67fab4a0c4c3a4f
jres <ljresetl@gmail.com>
2025-08-21 07:57:19 +0200

    up

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -12,12 +12,6 @@ const lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
 });
 
-// Змінні для пагінації
-let currentPage = 1;
-const perPage = 12;
-
-// ------------------- Утиліти -------------------
-
 /**
  * Додає зображення у галерею
  * @param {Array} images
@@ -79,65 +73,3 @@ export function showLoadMoreButton() {
 export function hideLoadMoreButton() {
   loadMoreBtn.classList.add("hidden");
 }
-
-/**
- * Затримка у мілісекундах
- * @param {number} ms
- */
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-// ------------------- Логіка завантаження -------------------
-
-async function fetchImages(page = 1) {
-  showLoader();
-  hideLoadMoreButton();
-
-  try {
-    // Робимо запит до API
-    const response = await fetch(
-      `https://pixabay.com/api/?key=51734453-5d46674fc0c6d7944706aca6e&q=flowers&page=${page}&per_page=${perPage}`
-    );
-    const data = await response.json();
-
-    // Мінімальна затримка 5 секунд для спінера
-    await sleep(5000);
-
-    return data.hits;
-  } catch (error) {
-    console.error(error);
-    return [];
-  } finally {
-    hideLoader();
-  }
-}
-
-async function initGallery() {
-  clearGallery();
-  currentPage = 1;
-  const images = await fetchImages(currentPage);
-  createGallery(images);
-
-  if (images.length === perPage) {
-    showLoadMoreButton();
-  }
-}
-
-async function loadMore() {
-  currentPage += 1;
-  const images = await fetchImages(currentPage);
-  createGallery(images);
-
-  if (images.length < perPage) {
-    hideLoadMoreButton();
-  } else {
-    showLoadMoreButton();
-  }
-}
-
-// Події
-loadMoreBtn.addEventListener("click", loadMore);
-
-// Запуск
-initGallery();
